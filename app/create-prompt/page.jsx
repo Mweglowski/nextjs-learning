ab31398557a88f29f6a4f44dbf3f9106dbd75e47
Marcin Weglowski <marcin.weglowski1@gmail.com>
2023-06-26 14:30:38 +0200

    api route

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -6,6 +6,9 @@ import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
@@ -15,13 +18,26 @@ const CreatePrompt = () => {
   const createPrompt = async (e) => {
     e.preventDefault();
 
-		setSubmitting(true);
+    setSubmitting(true);
 
-		try {
-			
-		} catch (error) {
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
 
-		}
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
   };
 
   return (
